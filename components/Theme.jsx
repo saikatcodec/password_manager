20bfa9256a9247cc11fe2187a17941b7ec727a88
Saikat Acharjee Joy <joy29940@gmail.com>
2022-06-28 19:28:25 +0600

    Changing the whole design of layout

--- components/Theme.jsx
+++ components/Theme.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import styles from "../styles/Theme.module.css";
 
-function Theme() {
-  return (
-    <div>Theme</div>
-  )
+function Theme({ children }) {
+    return (
+        <>
+            <div className={styles.container}>
+                <div className={styles.left}>
+                    <div className={styles.accnt_info}>
+                        <div className={styles.heading}>
+                            <span className={styles.logo}>P</span>
+                            Password Manager
+                        </div>
+                    </div>
+                    <div className={styles.nav_menu}>
+                        <ul className={styles.nav_items}>
+                            <li className={styles.item}>
+                                <Link href={"/"}>
+                                    <a>Home</a>
+                                </Link>
+                            </li>
+                            <li className={styles.item}>
+                                <Link href={"/"}>
+                                    <a>Password</a>
+                                </Link>
+                            </li>
+                            <li className={styles.item}>
+                                <Link href={"/"}>
+                                    <a>About</a>
+                                </Link>
+                            </li>
+                            <li className={styles.item}>
+                                <Link href={"/"}>
+                                    <a>Setting</a>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={styles.right}>{children}</div>
+            </div>
+        </>
+    );
 }
 
-export default Theme
+export default Theme;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import Theme from "../components/Theme";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Theme>
+                <Component {...pageProps} />
+            </Theme>
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
