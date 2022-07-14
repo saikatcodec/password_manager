d1c98f43302fbfa75889867ed595a950c1424e0d
Saikat Acharjee Joy <joy29940@gmail.com>
2022-07-14 16:39:42 +0600

    designing the login component

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import { FaGoogle, FaFacebook, FaApple } from "react-icons/fa";
 import styles from "../styles/Login.module.css";
 
 function Login() {
@@ -7,25 +8,46 @@ function Login() {
         <div className={styles.login_container}>
             <h3 className={styles.heading}>SIGNIN</h3>
             <form method="post" className={styles.form}>
-                <input
-                    type="text"
-                    className={styles.username}
-                    name="username"
-                    placeholder="Username"
-                />
-                <input
-                    type="password"
-                    className={styles.password}
-                    name="password"
-                    placeholder="Password"
-                />
+                <span className={styles.username_icon}>
+                    <input
+                        type="text"
+                        className={styles.username}
+                        name="username"
+                        placeholder="Username"
+                    />
+                </span>
+                <span className={styles.password_icon}>
+                    <input
+                        type="password"
+                        className={styles.password}
+                        name="password"
+                        placeholder="Password"
+                    />
+                </span>
                 <button className={styles.submit_btn} type="submit">
                     SIGNIN
                 </button>
             </form>
+            <div className={styles.forget}>
+                <Link href={"/forget_password"}>
+                    <a>Forget Password?</a>
+                </Link>
+            </div>
             <div className={styles.another_login}>
                 <Link href={"/"}>
-                    <a></a>
+                    <a>
+                        <FaGoogle />
+                    </a>
+                </Link>
+                <Link href={"/"}>
+                    <a>
+                        <FaFacebook />
+                    </a>
+                </Link>
+                <Link href={"/"}>
+                    <a>
+                        <FaApple />
+                    </a>
                 </Link>
             </div>
         </div>
---
+++ pages/forget_password.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function forget_password() {
+  return (
+    <div>forget_password</div>
+  )
+}
+
+export default forget_password
